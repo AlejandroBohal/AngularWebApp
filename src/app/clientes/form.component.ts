5e0a91aa784ebce85a1d00f3ab443199c8149c36
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-06-14 02:12:37 -0500

    Sp02us002Formulario-alert
    Formulario alerts

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router'
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -15,7 +17,11 @@ export class FormComponent implements OnInit {
   }
   public create(): void{
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+      cliente => {
+        swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito`, 'success');
+        this.router.navigate(['/clientes'])
+
+      }
     )
   }
 
