309a00b7dc0dd4c3d5faadd738dd9b17086d4c3a
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-06-14 01:42:46 -0500

    Sp02us002Formulario-Crear
    Formulario creación

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router'
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+})
+export class FormComponent implements OnInit {
+  cliente: Cliente = new Cliente()
+  titulo: string = "Crear cliente"
+  constructor(private clienteService: ClienteService, private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  public create(): void{
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
+  }
+
+}
